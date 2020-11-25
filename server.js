58498f4ec10105e88e9d1842ab82deddaefcf5cd
Daniel Hogg <Dan@Daniels-MacBook-Pro-2.local>
2020-11-25 15:20:38 +0000

    first sttempt at making a working post request

--- server.js
+++ server.js
@@ -3,8 +3,21 @@ const app = express()
 
 const PORT = 3000
 
+let envolope = {id: 0, title: food, budget: 100, }
+
+const envolopes = []
+let nextId = 1
+let totalBudget = 0
+
 app.get('/', (req, res) => {
     res.send('Hello, World!')
+})
+
+app.post('/envolopes', (req, res) => {
+    const newEnvolope = req.body
+    newEnvolope.id = nextId++
+    envolopes.push(newEnvolope)
+    res.status(201).send(newEnvolope)
 })
 
 app.listen(PORT, function() {
