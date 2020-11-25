430647606015c46e1830e7422818aa1cdfdf2cc2
Daniel Hogg <Dan@Daniels-MacBook-Pro-2.local>
2020-11-25 03:06:51 +0000

    Working get to ensure app setup

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
 const express = require('express')
 const app = express()
 
-const port = 3000
+const PORT = 3000
 
 app.get('/', (req, res) => {
-    console.log('Hello, World!')
+    res.send('Hello, World!')
+})
+
+app.listen(PORT, function() {
+    console.log(`Listening on port: ${PORT}`)
 })
