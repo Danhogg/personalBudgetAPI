5d9f559223f11ff9773d60ded7652e21b961df71
Daniel Hogg <Dan@Daniels-MacBook-Pro-2.local>
2020-11-26 02:16:48 +0000

    added function for validating an envolope

--- server.js
+++ server.js
@@ -1,19 +1,29 @@
 const express = require('express')
 const app = express()
 
-const PORT = 3000
+const PORT = process.env.PORT || 3000
 
 let envolope = {id: 0, title: 'food', budget: 100, }
 
-const envolopes = []
+let envolopes = []
+
 let nextId = 1
 let totalBudget = 0
+
+// validates that the given envolope is of a valid format
+function validateEnvolope(req, res, next) {
+    const newEnvolope = req.body
+    if (typeof newEnvolope.title !== 'string' || typeof newEnvolope.budget !== 'number') {
+        return res.status(400).send('Invalid envolope')
+    }
+    next()
+}
 
 app.get('/', (req, res) => {
     res.send('Hello, World!')
 })
 
-app.post('/envolopes', (req, res) => {
+app.post('/envolopes', validateEnvolope, (req, res) => {
     const newEnvolope = req.body
     newEnvolope.id = nextId++
     envolopes.push(newEnvolope)
