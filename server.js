bd4b08616237455708b114792d5f138f71a8dac1
Daniel Hogg <Dan@Daniels-MacBook-Pro-2.local>
2020-11-26 03:05:48 +0000

    working post for new envolopes and can validate them

--- server.js
+++ server.js
@@ -16,16 +16,18 @@ app.use(bodyParser.json())
 // validates that the given envolope is of a valid format
 function validateEnvolope(req, res, next) {
     const newEnvolope = req.body
-    if (typeof newEnvolope.title !== 'string' || typeof newEnvolope.budget !== 'number') {
+    if (typeof newEnvolope.title !== "string" || typeof newEnvolope.budget !== "number") {
         return res.status(400).send('Invalid envolope')
     }
     next()
 }
 
+//This was a test to ensure a get request could be made and the server was set up properly
 app.get('/', (req, res) => {
     res.send('Hello, World!')
 })
 
+//post method for adding an envolope to our envolopes array and will update to totalBudget
 app.post('/envolopes/', validateEnvolope, (req, res) => {
     const newEnvolope = req.body
     newEnvolope.id = nextId++
@@ -33,6 +35,7 @@ app.post('/envolopes/', validateEnvolope, (req, res) => {
     res.status(201).send(newEnvolope)
 })
 
+//starts the server listening on the desired port and states the port being listened to
 app.listen(PORT, function() {
     console.log(`Listening on port: ${PORT}`)
 })
