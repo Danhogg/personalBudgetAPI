34753d75c48fbd954ace0d71031bb1a03c4cbfdb
Daniel Hogg <Dan@Daniels-MacBook-Pro-2.local>
2020-11-27 01:19:34 +0000

    began a new method for updating envolopes

--- server.js
+++ server.js
@@ -1,15 +1,18 @@
 const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
+const { EDESTADDRREQ } = require('constants')
 
 const PORT = process.env.PORT || 3000
 
 //let envelope = {id: 0, title: 'food', budget: 100, }
 
+
+//create variables for envelope array(array of objects) and the nextId used for the 
+//id of the next envelope created
 let envelopes = []
 
 let nextId = 1
-let totalBudget = 0
 
 app.use(bodyParser.json())
 
@@ -22,18 +25,19 @@ function validateEnvelope(req, res, next) {
     next()
 }
 
-//This was a test to ensure a get request could be made and the server was set up properly
+//This sends a message to the user about what this app can do and what it does
 app.get('/', (req, res) => {
-    res.send('Hello, World!')
+    res.send('Hello, World! This API allows you to create a personal budget and use the envelope budgeting method.')
 })
 
-//post method for adding an envelope to our envelopes array and will update to totalBudget
+//post method for adding an envelope to our envelopes array
 app.post('/envelopes/', validateEnvelope, (req, res) => {
     const newEnvelope = req.body
     newEnvelope.id = nextId++
     envelopes.push(newEnvelope)
     res.status(201).send(newEnvelope)
 })
+
 
 //get method for all envelopes that have been created
 app.get('/envelopes/all', (req, res) => {
@@ -54,6 +58,13 @@ app.get('/envelopes/:id', (req, res) => {
         res.send(envelopes[req.envelopeIndex])
     }
 })
+
+app.put('/envelopes/:id', (req, res) => {
+    const newEnvelope = req.body
+    const envelopeId = Number(req.params.id)
+
+})
+
 //starts the server listening on the desired port and states the port being listened to
 app.listen(PORT, function() {
     console.log(`Listening on port: ${PORT}`)
